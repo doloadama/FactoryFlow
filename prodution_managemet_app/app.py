585ed5e36d14012d6ea2134652195e75cc1efd2d
doloadama <doloadama@outlook.com>
2023-07-07 11:44:38 +0000

    circular import

--- prodution_managemet_app/app.py
+++ prodution_managemet_app/app.py
@@ -1,5 +1,7 @@
+from flask import Flask
 from app import app
-from app import routes, models
+
+app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- prodution_managemet_app/app/__init__.py
+++ prodution_managemet_app/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from app import routes, models
 
 
 app = Flask(__name__)
