ee4c57dab5ec720716c16df0648ae3bea033a991
doloadama <doloadama@outlook.com>
2023-07-07 10:43:48 +0000

    .

--- prodution_managemet_app/app/__init__.py
+++ prodution_managemet_app/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///production.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes, models
--- prodution_managemet_app/app/models.py
+++ prodution_managemet_app/app/models.py
@@ -1,9 +1,42 @@
 from app import db
 
 class Order(db.Model):
+    date = db.Column(db.DateTimeField(auto_now_add=True))
     id = db.Column(db.Integer, primary_key=True)
     order_number = db.Column(db.String(50), unique=True)
-    # Define other fields for the Order model
-    
+    customer_name = db.Column(db.String(100))
+    quantity_ordered = db.Column(db.Integer)
+    livred = db.Column(db.Boolean, default=False)
+
     def __repr__(self):
         return f"<Order {self.order_number}>"
+
+class Inventory(db.Model):
+    date = db.Column(db.DateTimeField(auto_now_add=True))
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100))
+    quantity_in_stock = db.Column(db.Integer)
+    quantity_in_input = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Inventory {self.product_name}>"
+
+class Schedule(db.Model):
+    date_created = db.Column(db.DateTimeField(auto_now_add=True))
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100))
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+
+    def __repr__(self):
+        return f"<Schedule {self.product_name}>"
+
+class Quality(db.Model):
+    date = db.Column(db.DateTimeField(auto_now_add=True))
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100))
+    quality_status = db.Column(db.String(20))
+    quality_issue = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f"<Quality {self.product_name}>"
