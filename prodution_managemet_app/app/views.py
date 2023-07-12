a671c0d92252a990028f28e28eda8c70723f2a18
doloadama <doloadama@outlook.com>
2023-07-12 12:29:03 +0000

    .

---
+++ prodution_managemet_app/app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def create_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'create_user.html', {'form': form})
--- prodution_managemet_app/production_management_app/urls.py
+++ prodution_managemet_app/production_management_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('create-user/', views.create_user, name='create_user'),
 ]
